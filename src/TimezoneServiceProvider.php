9da1450ec56e946bb66c91771844eee9db499048
Kyle Graham <kylegraham05@gmail.com>
2024-07-08 11:16:16 -0700

    Create TimezoneServiceProvider

---
+++ src/TimezoneServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace lynexer\LaravelLocalTimezone;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimezoneServiceProvider extends ServiceProvider {
+    protected $defer = false;
+
+    public function boot(): void {
+        // 
+    }
+
+    public function register(): void {
+        // 
+    }
+}
